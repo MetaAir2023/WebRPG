7433516cd722cac7c6211d1c4840b08a56860ce1
jeongjiwon <jeewonre@hufs.ac.kr>
2023-06-22 14:32:21 +0900

    project init

---
+++ src/test/java/com/ohgiraffers/webrpg/WebrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.webrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
