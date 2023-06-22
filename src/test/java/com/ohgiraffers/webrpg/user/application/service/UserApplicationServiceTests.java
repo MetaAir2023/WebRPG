039f6356ff6f906c1b05803f452c20726aefd013
jeongjiwon <jeewonre@hufs.ac.kr>
2023-06-22 22:52:02 +0900

    [#19] 사용자 강화 상태 응답 구현

---
+++ src/test/java/com/ohgiraffers/webrpg/user/application/service/UserApplicationServiceTests.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.webrpg.user.application.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserApplicationServiceTests {
+}
