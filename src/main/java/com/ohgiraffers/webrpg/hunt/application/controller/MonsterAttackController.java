904cdf75652d0bfee5bffba38fe799b152e9bcf7
LAPTOP-O91Q221D\user <pyunghun120@gmail.com>
2023-06-22 17:18:54 +0900

    [Hunt] 패키지 구조 설정-1

---
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/controller/MonsterAttackController.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.webrpg.hunt.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("monsterattack")
+public class MonsterAttackController {
+}
