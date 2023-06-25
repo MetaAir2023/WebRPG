3ce4fc7a73541d3681bf3c57d437270492f81c9e
anichan <qufekf97@gmail.com>
2023-06-25 17:46:36 +0900

    랜덤몬스터 Service와 그를 위한 dto 작성

---
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/dto/RandomMonsterDTO.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.webrpg.hunt.application.dto;
+
+
+import com.ohgiraffers.webrpg.hunt.domain.aggregate.enumtype.MonsterET;
+import com.ohgiraffers.webrpg.hunt.domain.aggregate.vo.MonsterHp;
+import com.ohgiraffers.webrpg.hunt.domain.aggregate.vo.MonsterPower;
+import com.ohgiraffers.webrpg.hunt.domain.aggregate.vo.RewardExp;
+import com.ohgiraffers.webrpg.hunt.domain.aggregate.vo.RewardMoney;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class RandomMonsterDTO {
+    private int sequence;
+    private String monsterName;
+    private MonsterHp monsterHp;
+    private MonsterPower monsterPower;
+    private RewardExp rewardExp;
+    private RewardMoney rewardMoney;
+    private MonsterET monElement;
+}
--- src/main/java/com/ohgiraffers/webrpg/hunt/application/service/MonsterAppearApplicationService.java
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/service/MonsterAppearApplicationService.java
@@ -1,9 +1,35 @@
 package com.ohgiraffers.webrpg.hunt.application.service;
 
+import com.ohgiraffers.webrpg.hunt.application.dto.RandomMonsterDTO;
+import com.ohgiraffers.webrpg.hunt.domain.aggregate.entity.Monster;
+import com.ohgiraffers.webrpg.hunt.domain.service.MonsterAppearDomainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class MonsterAppearApplicationService {
 
 
+    private final MonsterAppearDomainService monsterAppearDomainService;
 
+    @Autowired
+    public MonsterAppearApplicationService(MonsterAppearDomainService monsterAppearDomainService){
+        this.monsterAppearDomainService = monsterAppearDomainService;
+    }
+
+
+    public RandomMonsterDTO randomMonster(int mapNum){
+        RandomMonsterDTO randomMon = new RandomMonsterDTO();
+        Monster monster = monsterAppearDomainService.randomMapMonster(mapNum);
+        randomMon.setMonsterName(monster.getMonsterName());
+        randomMon.setMonsterHp(monster.getMonsterHp());
+        randomMon.setMonsterPower(monster.getMonsterPower());
+        randomMon.setRewardExp(monster.getRewardExp());
+        randomMon.setRewardMoney(monster.getRewardMoney());
+        randomMon.setMonElement(monster.getMonElement());
+        return randomMon;
+    }
 
 
 
