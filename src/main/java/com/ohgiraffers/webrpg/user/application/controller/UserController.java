bb312ee19f4a21784afc90e560c4a78e9142f6f5
jeongmin lee <96166174+max0901@users.noreply.github.com>
2023-06-25 21:04:49 +0900

    Revert "userController userinfo기능 추가"

--- src/main/java/com/ohgiraffers/webrpg/user/application/controller/UserController.java
+++ src/main/java/com/ohgiraffers/webrpg/user/application/controller/UserController.java
@@ -1,15 +1,11 @@
 package com.ohgiraffers.webrpg.user.application.controller;
 
 import com.ohgiraffers.webrpg.user.application.dto.UserInfoDTO;
-import com.ohgiraffers.webrpg.user.application.dto.UserStatDTO;
 import com.ohgiraffers.webrpg.user.application.dto.loginDTO;
 import com.ohgiraffers.webrpg.user.application.service.UserApplicationService;
-import com.ohgiraffers.webrpg.user.domain.aggregate.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
@@ -39,18 +35,5 @@ public class UserController {
 
         return "main/menu";
     }
-    @GetMapping("info")
-    public String info(HttpSession session, Model mv){
-//        System.out.println(session.getAttribute("userName"));
-        String userName= session.getAttribute("userName").toString();
-        UserInfoDTO userInfoDTO=userApplicationService.getInfo(userApplicationService.getUserByName(userName));
-//        mv.addAttribute("userInfoDTO",userInfoDTO);
-            mv.addAttribute("userTotalHp",userInfoDTO.getTotalHP());
-        mv.addAttribute("userLevel",userInfoDTO.getUserLevel());
-       mv.addAttribute("userTotalSTR",userInfoDTO.getTotalSTR());
-//        System.out.println("userInfoDTO = " + userInfoDTO.getTotalHP());
-        return  "hunt/huntSelectMap";
-    }
-
 
 }
