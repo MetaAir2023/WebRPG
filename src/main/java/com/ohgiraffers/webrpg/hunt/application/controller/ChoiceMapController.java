5030e2ae46c2783f8706cfe4a5dd784de01f9f7c
jeongmin lee <maxsdf@naver.com>
2023-06-26 17:55:16 +0900

    타임리프에맞게 주소 재설정

--- src/main/java/com/ohgiraffers/webrpg/hunt/application/controller/ChoiceMapController.java
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/controller/ChoiceMapController.java
@@ -3,7 +3,8 @@ package com.ohgiraffers.webrpg.hunt.application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
+@RequestMapping("choicemap")
 
 
 public class ChoiceMapController {
