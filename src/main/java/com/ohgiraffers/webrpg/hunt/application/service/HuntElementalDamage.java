c5ea6f5acb97e5a6bee8f923bee67d414c7b158c
JeongHeum Yeon <48685874+madirony@users.noreply.github.com>
2023-06-26 16:56:17 +0900

    [#66] 몬스터 속성 공격 구현 완료

--- src/main/java/com/ohgiraffers/webrpg/hunt/application/service/HuntElementalDamage.java
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/service/HuntElementalDamage.java
@@ -1,8 +1,10 @@
 package com.ohgiraffers.webrpg.hunt.application.service;
 
 import com.ohgiraffers.webrpg.hunt.application.dto.*;
+import com.ohgiraffers.webrpg.hunt.domain.aggregate.enumtype.MonsterET;
 import com.ohgiraffers.webrpg.hunt.domain.repository.DomainRepository;
 import com.ohgiraffers.webrpg.hunt.domain.service.HuntExtraElemental;
+import com.ohgiraffers.webrpg.user.domain.aggregate.enumtype.ElementalType;
 import com.ohgiraffers.webrpg.user.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +28,7 @@ public class HuntElementalDamage {
                                         GetElementalDTO getElementalDTO,
                                         MonsterStrDTO monsterStrDTO){
         ElementDamageDTO damageDTO = new ElementDamageDTO();
-        int monATK = noEtDmg.getMonsterATK();
+        double monATK = noEtDmg.getMonsterATK() * monsterPercentage(sequence, getElementalDTO);
         double userATK = noEtDmg.getUserATK() * totalPercentage(sequence,getElementalDTO);
 
         damageDTO.setMonsterATK(monATK);
@@ -46,16 +48,16 @@ public class HuntElementalDamage {
             return 1;
         }
         return userUpLevel * 0.1;
-
-
     }
 
-//    public int additionalMonsterElementATK(MonsterAttackDTO monsterAttackDTO, GetElementalDTO getElementalDTO){
-//        Enum monsterElement = getElementalDTO.getMonET();
-//        Enum userElement = getElementalDTO.getUserET();
-//        if(monsterElement)
-//
-//    }
+    public double monsterPercentage(int sequence, GetElementalDTO getElementalDTO){
 
-
+        if(huntExtraElemental.compareET(getElementalDTO)){
+            return 0.9;
+        }
+        if(huntExtraElemental.ElementCompatibility(sequence,getElementalDTO)){
+            return 1;
+        }
+        return 1.1;
+    }
 }
