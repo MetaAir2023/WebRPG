6b0b80c05eae7807b7b2103d0495e28d7d9f534d
LAPTOP-O91Q221D\user <pyunghun120@gmail.com>
2023-06-26 14:48:50 +0900

    [Hunt] 컨트롤러, 서비스 부분 추가

---
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/dto/IntegrateUserAttackDTO.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.webrpg.hunt.application.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class IntegrateUserAttackDTO {
+
+    UserAttackDTO userAttackDTO;
+    UserPatternDTO userPatternDTO;
+
+}
---
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/dto/UserPatternDTO.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.webrpg.hunt.application.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserPatternDTO {
+
+    private int attackCnt;
+    private int heal;
+
+}
