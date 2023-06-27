2ab2d41ef2a55da71dccb527cc137c0ef9bf0185
anichan <qufekf97@gmail.com>
2023-06-27 09:42:55 +0900

    MonsterAppearController 수정

--- src/main/java/com/ohgiraffers/webrpg/hunt/application/controller/MonsterAppearController.java
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/controller/MonsterAppearController.java
@@ -33,7 +33,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt1";
-    }@GetMapping ("randomMonster2")
+    }
+    @GetMapping ("randomMonster2")
     public String monsterAppear2(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -44,7 +45,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt2";
-    }@GetMapping ("randomMonster3")
+    }
+    @GetMapping ("randomMonster3")
     public String monsterAppear3(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -55,7 +57,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt3";
-    }@GetMapping ("randomMonster4")
+    }
+    @GetMapping ("randomMonster4")
     public String monsterAppear4(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -66,7 +69,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt4";
-    }@GetMapping ("randomMonster5")
+    }
+    @GetMapping ("randomMonster5")
     public String monsterAppear5(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -77,7 +81,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt5";
-    }@GetMapping ("randomMonster6")
+    }
+    @GetMapping ("randomMonster6")
     public String monsterAppear6(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -88,7 +93,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt6";
-    }@GetMapping ("randomMonster7")
+    }
+    @GetMapping ("randomMonster7")
     public String monsterAppear7(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -99,7 +105,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt7";
-    }@GetMapping ("randomMonster8")
+    }
+    @GetMapping ("randomMonster8")
     public String monsterAppear8(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -110,7 +117,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt8";
-    }@GetMapping ("randomMonster9")
+    }
+    @GetMapping ("randomMonster9")
     public String monsterAppear9(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
@@ -121,7 +129,8 @@ public class MonsterAppearController {
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
 
         return "hunt/hunt9";
-    }@GetMapping ("randomMonster10")
+    }
+    @GetMapping ("randomMonster10")
     public String monsterAppear10(@RequestParam String mapNum, Model model){
         int mapNumber = Integer.parseInt(mapNum);
 
