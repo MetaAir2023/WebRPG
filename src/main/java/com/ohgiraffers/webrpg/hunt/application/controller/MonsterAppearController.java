0315e395bea697b1b3d43db9369d3540aeec2489
jeongmin lee <maxsdf@naver.com>
2023-06-27 15:24:49 +0900

    randommonster기능 수정

--- src/main/java/com/ohgiraffers/webrpg/hunt/application/controller/MonsterAppearController.java
+++ src/main/java/com/ohgiraffers/webrpg/hunt/application/controller/MonsterAppearController.java
@@ -2,6 +2,8 @@ package com.ohgiraffers.webrpg.hunt.application.controller;
 
 import com.ohgiraffers.webrpg.hunt.application.dto.RandomMonsterDTO;
 import com.ohgiraffers.webrpg.hunt.application.service.MonsterAppearApplicationService;
+import com.ohgiraffers.webrpg.user.application.dto.UserInfoDTO;
+import com.ohgiraffers.webrpg.user.application.service.UserApplicationService;
 import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,23 +18,32 @@ import static javax.swing.text.html.CSS.getAttribute;
 @RequestMapping("monsterappear")
 public class MonsterAppearController {
     private final MonsterAppearApplicationService monsterAppearApplicationService;
-
+    private final UserApplicationService userApplicationService;
     @Autowired
-    public MonsterAppearController(MonsterAppearApplicationService monsterAppearApplicationService){
+    public MonsterAppearController(MonsterAppearApplicationService monsterAppearApplicationService,UserApplicationService userApplicationService){
         this.monsterAppearApplicationService = monsterAppearApplicationService;
+        this.userApplicationService=userApplicationService;
     }
 
     @GetMapping ("randomMonster1")
-    public String monsterAppear1(@RequestParam String mapNum, Model model){
+    public String monsterAppear1(HttpSession session ,@RequestParam String mapNum, Model model){
+        String userName= session.getAttribute("userName").toString();
+        UserInfoDTO userInfoDTO=userApplicationService.getInfo(userApplicationService.getUserByName(userName));
+
         int mapNumber = Integer.parseInt(mapNum);
 
         RandomMonsterDTO randomMonsterDTO = monsterAppearApplicationService.randomMonster(mapNumber);
-        model.addAttribute("monsterHp",randomMonsterDTO.getMonsterHp());
-        model.addAttribute("monsterPower",randomMonsterDTO.getMonsterPower());
+        model.addAttribute("monsterHp",randomMonsterDTO.getMonsterHp().getValue());
+        model.addAttribute("monsterPower",randomMonsterDTO.getMonsterPower().getValue());
         model.addAttribute("monsterName",randomMonsterDTO.getMonsterName());
         model.addAttribute("monsterElementalType",randomMonsterDTO.getMonsterElementalType());
-
-        return "hunt/hunt1";
+        model.addAttribute("monsterSequence",randomMonsterDTO.getSequence());
+        model.addAttribute("userName",userInfoDTO.getName());
+        model.addAttribute("userTotalHp",userInfoDTO.getTotalHP());
+        model.addAttribute("userTotalStr",userInfoDTO.getTotalSTR());
+        model.addAttribute("userLevel",userInfoDTO.getUserLevel());
+        model.addAttribute("userType",userInfoDTO.getElementalType());
+        return "hunt/huntmaps/hunt1";
     }
     @GetMapping ("randomMonster2")
     public String monsterAppear2(@RequestParam String mapNum, Model model){
