6174a40b402b6746387047f9aa9282a4e4d47c9a
anichan <qufekf97@gmail.com>
2023-06-29 10:29:40 +0900

    불필요 테스트 삭제

--- src/test/java/com/ohgiraffers/webrpg/hunt/application/service/MonsterAttackApplicationServiceTests.java
+++ src/test/java/com/ohgiraffers/webrpg/hunt/application/service/MonsterAttackApplicationServiceTests.java
@@ -51,56 +51,7 @@ public class MonsterAttackApplicationServiceTests {
         assertEquals("소드마스터", monsterAttack.getUser().getName());
     }
 
-    @Test
-    @DisplayName("몬스터가 유저를 단 한번 공격하는 속성 공격 Test - 몬스터의 속성이 열세일 경우, 약화된 공격")
-    public void testAttackToUser(){
-        MonsterDTO monster = monsterAttackApplicationService.getInfo(monsterInfraRepository.findMonsterBySequence(1));
-        User user = userApplicationService.getUserBySequence(1);
-        UserInfoDTO userInfo = userApplicationService.getInfo(user);
-        int sequence = 1;
-        GetElementalDTO getElementalDTO = new GetElementalDTO();
-        MonsterET monsterET = MonsterET.WATER;
-        ElementalType userET = ElementalType.GRASS;
-        getElementalDTO.setMonET(monsterET);
-        getElementalDTO.setUserET(userET);
-        MonsterAttackDTO monsterAttack = monsterAttackApplicationService.initMonsterAttackDTO(monster, userInfo);
-        monsterAttack = monsterAttackApplicationService.attackToUser(monsterAttack, sequence, getElementalDTO);
-        assertEquals(920, monsterAttack.getUserCurrentHP());
-    }
 
-    @Test
-    @DisplayName("몬스터가 유저를 단 한번 공격하는 속성 공격 Test - 몬스터의 속성이 동등할 경우, 일반 공격")
-    public void testAttackToUser2(){
-        MonsterDTO monster = monsterAttackApplicationService.getInfo(monsterInfraRepository.findMonsterBySequence(1));
-        User user = userApplicationService.getUserBySequence(1);
-        UserInfoDTO userInfo = userApplicationService.getInfo(user);
-        int sequence = 1;
-        GetElementalDTO getElementalDTO = new GetElementalDTO();
-        MonsterET monsterET = MonsterET.WATER;
-        ElementalType userET = ElementalType.WATER;
-        getElementalDTO.setMonET(monsterET);
-        getElementalDTO.setUserET(userET);
-        MonsterAttackDTO monsterAttack = monsterAttackApplicationService.initMonsterAttackDTO(monster, userInfo);
-        monsterAttack = monsterAttackApplicationService.attackToUser(monsterAttack, sequence, getElementalDTO);
-        assertEquals(900, monsterAttack.getUserCurrentHP());
-    }
-
-    @Test
-    @DisplayName("몬스터가 유저를 단 한번 공격하는 속성 공격 Test - 몬스터의 속성이 우세할 경우, 강화된 공격")
-    public void testAttackToUser3(){
-        MonsterDTO monster = monsterAttackApplicationService.getInfo(monsterInfraRepository.findMonsterBySequence(1));
-        User user = userApplicationService.getUserBySequence(1);
-        UserInfoDTO userInfo = userApplicationService.getInfo(user);
-        int sequence = 1;
-        GetElementalDTO getElementalDTO = new GetElementalDTO();
-        MonsterET monsterET = MonsterET.WATER;
-        ElementalType userET = ElementalType.FIRE;
-        getElementalDTO.setMonET(monsterET);
-        getElementalDTO.setUserET(userET);
-        MonsterAttackDTO monsterAttack = monsterAttackApplicationService.initMonsterAttackDTO(monster, userInfo);
-        monsterAttack = monsterAttackApplicationService.attackToUser(monsterAttack, sequence, getElementalDTO);
-        assertEquals(880, monsterAttack.getUserCurrentHP());
-    }
 
     @Test
     @DisplayName("몬스터가 유저를 3번 공격했을 경우, 그 다음 공격 패턴은 -두번 공격-")
@@ -123,38 +74,5 @@ public class MonsterAttackApplicationServiceTests {
         assertEquals(880, integrateMonsterAttackDTO.getMonsterAttackDTO().getUserCurrentHP());
     }
 
-//    ***** 참고용 *****
-//    속성 공격 추가 이전 테스트 코드
-//    @Test
-//    @DisplayName("몬스터가 유저를 3번 공격했을 경우, 그 다음 공격 패턴은 -두번 공격- 공격 카운트 덧셈이 올바르게 작동하는지 확인")
-//    public void testMonsterPatternDoubleAttackCheckAttackCnt(){
-//        MonsterDTO monsterDTO = monsterAttackApplicationService.getInfo(monsterInfraRepository.findMonsterBySequence(2));
-//        monsterDTO.setMonsterPower(100);
-//        User user = userApplicationService.getUserBySequence(1);
-//        UserInfoDTO userInfo = userApplicationService.getInfo(user);
-//        MonsterAttackDTO monsterAttackDTO = monsterAttackApplicationService.initMonsterAttackDTO(monsterDTO, userInfo);
-//        MonsterPatternDTO monsterPatternDTO = monsterAttackApplicationService.initMonsterPatternDTO();
-//        monsterPatternDTO.setAttackCnt(3);
-//        IntegrateMonsterAttackDTO integrateMonsterAttackDTO = monsterAttackApplicationService.initIntegrateMonsterAttackDTO(monsterAttackDTO, monsterPatternDTO);
-//        integrateMonsterAttackDTO = monsterAttackApplicationService.attackPattern(integrateMonsterAttackDTO);
-//        assertEquals(5, integrateMonsterAttackDTO.getMonsterPatternDTO().getAttackCnt());
-//    }
-//
-//    @Test
-//    @DisplayName("최대 HP가 100인 몬스터가 체력 30%이하로 깎였을 경우에, 회복 패턴 실시 (단, 회복 패턴은 한번만 실행된다.)")
-//    public void testMonsterPatternHeal(){
-//        MonsterDTO monsterDTO = monsterAttackApplicationService.getInfo(monsterInfraRepository.findMonsterBySequence(2));
-//        monsterDTO.setMonsterPower(100);
-//        User user = userApplicationService.getUserBySequence(1);
-//        UserInfoDTO userInfo = userApplicationService.getInfo(user);
-//        MonsterAttackDTO monsterAttackDTO = monsterAttackApplicationService.initMonsterAttackDTO(monsterDTO, userInfo);
-//        MonsterPatternDTO monsterPatternDTO = monsterAttackApplicationService.initMonsterPatternDTO();
-//        monsterPatternDTO.setAttackCnt(3);
-//        monsterAttackDTO.setMonsterCurrentHP(29);
-//        IntegrateMonsterAttackDTO integrateMonsterAttackDTO = monsterAttackApplicationService.initIntegrateMonsterAttackDTO(monsterAttackDTO, monsterPatternDTO);
-//        integrateMonsterAttackDTO = monsterAttackApplicationService.attackPattern(integrateMonsterAttackDTO);
-//        assertEquals(39, integrateMonsterAttackDTO.getMonsterAttackDTO().getMonsterCurrentHP());
-//    }
-//    ***** 속성 공격 추가 이전 테스트 코드 *****
 
 }
